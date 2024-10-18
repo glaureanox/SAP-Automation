088459a5a66b4c2d103681caccb8613273b08b08
glaureanox <gbi.laureano07@gmail.com>
2024-10-18 16:44:38 -0300

    fixed bugs

--- ih03_infos.py
+++ ih03_infos.py
@@ -5,7 +5,7 @@ import pyautogui as pg
 def open_transaction():
     while True:
         try:
-            search_transaction = pg.locateOnScreen("./img/search_transaction.png", confidence=0.8)
+            search_transaction = pg.locateOnScreen("./img/first_part/search_transaction.png", confidence=0.8)
             
             center = pg.center(search_transaction)
             
@@ -22,7 +22,7 @@ def open_transaction():
 def insert_inventory(): # inserir o inventario
     while True:
         try:
-            inventory_text_area = pg.locateOnScreen("./img/inventory/insert_inventory.png", confidence=0.7)
+            inventory_text_area = pg.locateOnScreen("./img/first_part/inventory/insert_inventory.png", confidence=0.7)
             
             center = pg.center(inventory_text_area)
             
@@ -40,10 +40,10 @@ def find_square_and_check(): # preencher a checklist
     while True:
         try:
             time.sleep(1)
-            checklist_screen = pg.locateOnScreen("./img/explosion.png", confidence=0.8)
+            checklist_screen = pg.locateOnScreen("./img/first_part/explosion.png", confidence=0.8)
             
             if checklist_screen: 
-                checklist_square = pg.locateAllOnScreen("./img/square_checklist.png", region=checklist_screen, confidence=0.8)
+                checklist_square = pg.locateAllOnScreen("./img/first_part/square_checklist.png", region=checklist_screen, confidence=0.8)
                 for box in checklist_square:
                     cntr_sqr = pg.center(box)
                     pg.click(cntr_sqr)
@@ -57,7 +57,7 @@ def find_square_and_check(): # preencher a checklist
 def send_informations(): # enviar as informacoes preenchidas
     while True:
         try:
-            clock = pg.locateOnScreen("./img/send_btn.png", confidence=0.8)
+            clock = pg.locateOnScreen("./img/first_part/send_btn.png", confidence=0.8)
             
             if clock:
                 center = pg.center(clock)
